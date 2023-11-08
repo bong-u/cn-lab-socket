9610d7fe2332f3af13c4971e75f0163a4530cbe5
bong-u <bongudev@gmail.com>
2023-11-08 16:32:43 +0900

    modify message5

--- tcp-server.py
+++ tcp-server.py
@@ -15,7 +15,7 @@ while 1:
     print('Client address:', addr)
     data = conn.recv(BUFFER_SIZE)
 
-    currentTime = ' ' + time.ctime(time.time()) + '\r\n'
+    currentTime = ' (modified server) ' + time.ctime(time.time()) + '\r\n'
     print(data.decode('utf-8'))
     data = data + currentTime.encode('ascii')
     conn.send(data)
